c22aca54c7de1eb2042d4da48c0de35e1906f6e8
unknown <marja.marinic@gmail.com>
2021-06-05 02:37:45 +0100

    add: shuffled squares to the board & change: shuffleSquare naming policy

--- assets/js/minesweeper.js
+++ assets/js/minesweeper.js
@@ -4,22 +4,25 @@ let bombCount = 10;
 let squares = [];
 
 // Add 'bombs' and 'empty fields' to Array amd shuffle it's value
-function shuffleBombs() {
-    const bombArray = Array(bombCount).fill('boom');
+function shuffleSquares() {
+    const bombArray = Array(bombCount).fill('bomb');
     const emptyArray = Array(width * width - bombCount).fill('empty');
     let boardArray = emptyArray.concat(bombArray);
     
     boardArray = boardArray.sort(() => Math.random() -0.5);
-    console.log(boardArray);
+    
+    return boardArray;
 }
 
 // Create board filled with width*width squares
 function createBoard() {
-    shuffleBombs();
+    const shuffledSquares = shuffleSquares();
+    console.log(shuffledSquares);
 
     for(let i = 0; i < width*width; i++) {
         const square = document.createElement('div');
         square.setAttribute('id', i);
+        square.classList.add(shuffledSquares[i]);
         board.appendChild(square);
         squares.push(square);
     }
