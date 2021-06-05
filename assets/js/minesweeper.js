4bc67d044f05f18463fe6c6fb21455930c83e412
unknown <marja.marinic@gmail.com>
2021-06-05 01:55:36 +0100

    add: minesweeper grid with 100 squares 0-99

---
+++ assets/js/minesweeper.js
@@ -0,0 +1,16 @@
+const board = document.querySelector('.board');
+let width = 10; //let's say the game board will be 10*10 squares
+let squares = [];
+
+// Create board filled with squares
+function createBoard() {
+    for(let i = 0; i < width*width; i++) {
+        const square = document.createElement('div'); //create width*width squares
+        square.setAttribute('id', i);
+        board.appendChild(square);
+        squares.push(square);
+    }
+}
+
+createBoard();
+
