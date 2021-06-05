a0d4f6738110409808b196edb4c203d8ad8a4b46
unknown <marja.marinic@gmail.com>
2021-06-05 01:58:29 +0100

    fix: comment

--- assets/js/minesweeper.js
+++ assets/js/minesweeper.js
@@ -2,7 +2,7 @@ const board = document.querySelector('.board');
 let width = 10; //let's say the game board will be 10*10 squares
 let squares = [];
 
-// Create board filled with squares
+// Create board filled with width*width squares
 function createBoard() {
     for(let i = 0; i < width*width; i++) {
         const square = document.createElement('div'); //create width*width squares
