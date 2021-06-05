1784f9bfc671ffef546a615be13c00f7732f130d
unknown <marja.marinic@gmail.com>
2021-06-05 02:24:58 +0100

    add: shuffle Bomb function

--- assets/js/minesweeper.js
+++ assets/js/minesweeper.js
@@ -1,16 +1,31 @@
 const board = document.querySelector('.board');
-let width = 10; //let's say the game board will be 10*10 squares
+let width = 10;
+let bombCount = 10;
 let squares = [];
+
+// Add 'bombs' and 'empty fields' to Array amd shuffle it's value
+function shuffleBombs() {
+    const bombArray = Array(bombCount).fill('boom');
+    const emptyArray = Array(width * width - bombCount).fill('empty');
+    let boardArray = emptyArray.concat(bombArray);
+    
+    boardArray = boardArray.sort(() => Math.random() -0.5);
+    console.log(boardArray);
+}
 
 // Create board filled with width*width squares
 function createBoard() {
+    shuffleBombs();
+
     for(let i = 0; i < width*width; i++) {
-        const square = document.createElement('div'); //create width*width squares
+        const square = document.createElement('div');
         square.setAttribute('id', i);
         board.appendChild(square);
         squares.push(square);
     }
+    
 }
 
 createBoard();
+
 
