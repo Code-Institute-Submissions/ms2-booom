d330a91fd1e98ee510adbafaddb2a5215c3eb912
unknown <marja.marinic@gmail.com>
2021-06-06 01:39:58 +0100

    add: function to reveal squares with data val 0

--- assets/js/minesweeper.js
+++ assets/js/minesweeper.js
@@ -1,8 +1,32 @@
 const board = document.querySelector('.board');
-let width = 15;
+let width = 10;
 let height = 10;
-let bombCount = 20;
+let bombCount = 10;
 let squares = [];
+let gameOver = false;
+
+createBoard();
+
+// Create board filled with width*width squares
+function createBoard() {
+    const shuffledSquares = shuffleSquares();
+    console.log(shuffledSquares); // ----------------------------------CONSOLE LOG
+
+    for(let i = 0; i < width * height ; i++) {
+        const square = document.createElement('div');
+        square.setAttribute('id', i);
+        square.classList.add(shuffledSquares[i]);
+        board.appendChild(square);
+        squares.push(square);
+
+        // Add Event Listener for every board element
+        square.addEventListener('click', function(e) {
+            click(square);
+        })
+    }
+    
+    numOfBombsSurroundingEmpty();
+}
 
 // Add 'bombs' and 'empty fields' to Array amd shuffle it's value
 function shuffleSquares() {
@@ -46,31 +70,17 @@ function numOfBombsSurroundingEmpty() {
     }
 }
 
-// Create board filled with width*width squares
-function createBoard() {
-    const shuffledSquares = shuffleSquares();
-    console.log(shuffledSquares); // ----------------------------------CONSOLE LOG
 
-    for(let i = 0; i < width * height ; i++) {
-        const square = document.createElement('div');
-        square.setAttribute('id', i);
-        square.classList.add(shuffledSquares[i]);
-        board.appendChild(square);
-        squares.push(square);
 
-        // Add Event Listener for every board element
-        square.addEventListener('click', function(e) {
-            click(square);
-        })
-    }
-    
-    numOfBombsSurroundingEmpty();
-}
 
-createBoard();
 
 // Game over click case
 function click(square) {
+    if (gameOver) 
+        return;
+    if (square.classList.contains('revealed-square') || square.classList.contains('flag'))
+        return;
+
     if (square.classList.contains('bomb')) {
         alert('Game Over!'); //----------------------------------------------alert
     } else {
@@ -82,8 +92,67 @@ function click(square) {
             return;
         }
 
+        revealSquare(square);
+
         square.classList.add('revealed-square');
     }
 }
 
+// Reveal squares with data value 0 with recursion
+function revealSquare(square) {
+    let currentId = square.id;
+    const leftBorder = (currentId % width === 0);
+    const rightBorder = (currentId % width === width - 1);
+
+    setTimeout(() => {
+        // Checking North direction
+        if (currentId > (width - 1)) {
+            const newId = parseInt(currentId) - width;
+            const newSquare = document.getElementById(newId);
+            click(newSquare);
+        }
+        // Checking East direction
+        if (currentId < (width * height - 1) && !rightBorder) {
+            const newId = parseInt(currentId) + 1;
+            const newSquare = document.getElementById(newId);
+            click(newSquare);
+        }
+        // Checking South direction
+        if (currentId < (width * height - width)) {
+            const newId = parseInt(currentId) + width;
+            const newSquare = document.getElementById(newId);
+            click(newSquare);
+        }
+        // Checking West direction
+        if (currentId > 0 && !leftBorder) {
+            const newId = parseInt(currentId) -1;
+            const newSquare = document.getElementById(newId);
+            click(newSquare);
+        }
+        // Checking North-East direction
+        if (currentId < (width * height - width - 1) && !rightBorder) {
+            const newId = parseInt(currentId) + 1 + width;
+            const newSquare = document.getElementById(newId);
+            click(newSquare);
+        } 
+        // Checking South-East direction
+        if (currentId > (width - 1) && !rightBorder) {
+            const newId = parseInt(currentId) + 1 - width;
+            const newSquare = document.getElementById(newId);
+            click(newSquare);
+        }
+        // Checking North-West direction
+        if (currentId < (width * height - width) && !leftBorder) {
+            const newId = parseInt(currentId) - 1 + width;
+            const newSquare = document.getElementById(newId);
+            click(newSquare);
+        }
+        // Checking South-West direction
+        if (currentId > width && !leftBorder) {
+            const newId = parseInt(currentId) - 1 - width;
+            const newSquare = document.getElementById(newId);
+            click(newSquare);
+        }
+    }, 50);
+}
 
