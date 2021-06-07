25f7c6083b96b984845b5279e133371a9ca8961c
unknown <marja.marinic@gmail.com>
2021-06-07 04:07:37 +0100

    add: bootstrap grid to minesweeper

--- assets/js/minesweeper.js
+++ assets/js/minesweeper.js
@@ -24,7 +24,7 @@ let squares = [];
 function bodyLoaded(){
         //https://stackoverflow.com/a/3540295
     if( /Android|webOS|iPhone|iPad|Mac|Macintosh|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-        width=10;
+        width = 8;
         height=15;
     }
     createBoard();
